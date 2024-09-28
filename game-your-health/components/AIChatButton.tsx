cbb11b767485ce79ef03297c5b96528a5e736b1e
Paweł Palacz <ppalacz@edu.cdv.pl>
2024-09-29 01:53:17 +0200

    AI chat button

---
+++ game-your-health/components/AIChatButton.tsx
@@ -0,0 +1,49 @@
+import {Pressable, StyleSheet} from "react-native";
+import {TabBarIcon} from "@/components/navigation/TabBarIcon";
+import {Colors} from "@/constants/Colors";
+import {Href, useRouter} from "expo-router";
+
+export default function AIChatButton() {
+  const router = useRouter()
+
+  return (
+    <Pressable
+      style={
+        ({pressed}) =>
+          [
+            styles.aiButton,
+            {
+              backgroundColor: pressed ? '#ffffffcc' : '#ffffff99'
+            }
+          ]
+      }
+      onPress={() => router.push('/(tabs)/ai-chat' as Href)}
+    >
+      {
+        ({ pressed }) => (
+            <TabBarIcon
+              name={'sparkles'}
+              color={pressed ? Colors.button.primary.pressedBackgorund : Colors.button.primary.background}
+            />
+        )
+      }
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+  aiButton: {
+    width: 55,
+    height: 55,
+    position: 'absolute',
+    bottom: 80,
+    right: 30,
+    borderRadius: 50,
+    borderCurve: 'continuous',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: "#fff"
+  }
+})
